a8d153b54c1a6700fb7677ecf887f68271094042
j0ker123 <s1010455011@fh-hagenberg.at>
2011-11-23 14:38:40 +0100

    error beim ausführen ^^

--- MIECCompiler/MiniIEC/main.cpp
+++ MIECCompiler/MiniIEC/main.cpp
@@ -1,6 +1,41 @@
+#include <stdio.h>
+#include <iostream>
 
+#include "Scanner.h"
+#include "Parser.h"
+
+
+using namespace MIEC;
+using namespace std;
 
 int main(int argc, char* argv[])
-{
-   return 0;
+{	
+	//FILE* pFile = fopen(argv[1], "r");
+
+	Parser* parser;
+	FILE* pFile;
+
+	for( int i = 1; i < argc; i++ )
+	{
+		pFile = fopen(argv[i], "r");
+
+		parser = new Parser( new Scanner( pFile ) );
+		parser->Parse();
+
+		cout << "parse file: " << argv[i] << endl;
+		cout << "count errors: " << parser->errors->count << endl;
+	}
+
+	
+	//Scanner* scan = new Scanner( pFile );
+	//
+	//Parser* parser = new Parser( scan );
+
+	//parser->Parse();
+
+	//cout << "count errors: " << parser->errors->count << endl;
+
+	getchar();
+
+	return 0;
 }
