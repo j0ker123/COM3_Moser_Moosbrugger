c56349afb67a1192f6c5677207235498ac2fac86
j0ker123 <s1010455011@students.fh-hagenberg.at>
2012-01-30 17:20:18 +0100

    added print method for dac

--- mieccompiler/MiniIEC/DACSymbol.cpp
+++ mieccompiler/MiniIEC/DACSymbol.cpp
@@ -50,4 +50,83 @@ Symbol* const DACSymbol::GetArgument2() const
 	return mpArg2;
 }
 
+const wchar_t* const DACSymbol::Print()
+{
+		const wchar_t* operation;
+
+		switch(this->mOperation) {
+		case DACSymbol::eAdd :
+			operation = coco_string_create(L"ADD"); break;
+		case DACSymbol::eSubtract :
+			operation = coco_string_create(L"SUB"); break;
+		case DACSymbol::eMultiply :
+			operation = coco_string_create(L"MULT"); break;
+		case DACSymbol::eDivide :
+			operation = coco_string_create(L"DIV"); break;
+		case DACSymbol::eAssign :
+			operation = coco_string_create(L"ASSIGN"); break;
+		case DACSymbol::eIfFalseJump :
+			operation = coco_string_create(L"IFFALSEJUMP"); break;
+		case DACSymbol::eIsNotEqual :
+			operation = coco_string_create("NOTEQUAL"); break;
+		case DACSymbol::eJump :
+			operation = coco_string_create(L"JUMP"); break;
+		case DACSymbol::eIfJump :
+			operation = coco_string_create(L"IFJUMP"); break;
+		case DACSymbol::ePrint :
+			operation = coco_string_create(L"PRINT"); break;
+		case DACSymbol::eIsEqual :
+			operation = coco_string_create(L"EQUAL"); break;
+		case DACSymbol::eIsGreater :
+			operation = coco_string_create(L"GREATER"); break;
+		case DACSymbol::eIsGreaterEqual :
+			operation = coco_string_create(L"GREATEREQUAL"); break;
+		case DACSymbol::eIsLess :
+			operation = coco_string_create(L"LESS"); break;
+		case DACSymbol::eIsLessEqual : 
+			operation = coco_string_create(L"LESSEQUAL"); break;
+		case DACSymbol::eExit :
+			operation = coco_string_create(L"EXIT"); break;
+		default:
+			operation = coco_string_create(L"UNKOWN"); break;
+		}
+
+		const wchar_t* returnVal = coco_string_create(this->GetName());
+		returnVal = coco_string_create_append(returnVal, L" =\n" );
+
+		if ( this->mpArg1 != NULL ) {
+			if ( wmemcmp( this->mpArg1->GetName(), L"$", 1) == 0 )
+			{
+				returnVal = coco_string_create_append(returnVal, this->mpArg1->GetName() );
+			} 
+			else
+			{
+				returnVal = coco_string_create_append(returnVal, L"Arg1: " );
+				returnVal = coco_string_create_append(returnVal, this->mpArg1->Print() );
+				returnVal = coco_string_create_append(returnVal, L" " );
+			}
+		}
+
+		returnVal = coco_string_create_append(returnVal, L" OP: ");
+		returnVal = coco_string_create_append(returnVal, operation );
+		returnVal = coco_string_create_append(returnVal, L" ");
+
+		if ( this->mpArg2 != NULL ) {
+			if ( wmemcmp( this->mpArg2->GetName(), L"$", 1) == 0 )
+			{
+				returnVal = coco_string_create_append(returnVal, this->mpArg2->GetName() );
+			} 
+			else
+			{
+				returnVal = coco_string_create_append(returnVal, L"Arg2: " );
+				returnVal = coco_string_create_append(returnVal, this->mpArg2->Print() );
+				returnVal = coco_string_create_append(returnVal, L" " );
+			}
+		}
+
+		delete operation, operation = 0;
+
+		return returnVal;
+}
+
 } // namespace MIEC
